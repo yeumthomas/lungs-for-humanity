b5111d14abde10df53f1eed0832540d6b1c08814
Thomas Yeum <thomasyeum@Thomas-MacBook-Pro.local>
2020-05-24 05:03:19 -0500

    Added Dependencies

--- src/main.js
+++ src/main.js
@@ -1,22 +1,26 @@
 import Vue from 'vue'
-import App from './App.vue';
-import Home from './Home.vue';
-import Donate from './Donate.vue';
-import Contact from './Contact.vue';
-import ApolloBVM from './ApolloBVM.vue';
+import App from './App.vue'
+import Home from './Home.vue'
+import Donate from './Donate.vue'
+import Contact from './Contact.vue'
+import ApolloBVM from './ApolloBVM.vue'
 
-import Element from 'element-ui';
-import 'element-ui/lib/theme-chalk/reset.css';
-import 'element-theme-chalk';
-import VueRouter from 'vue-router';
-import BootstrapVue from 'bootstrap-vue'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/reset.css'
+import 'element-theme-chalk'
+import VueRouter from 'vue-router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faFacebookSquare, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-Vue.use('element-theme-chalk');
-Vue.use(Element, { size: 'small', zIndex: 3000 });
-Vue.use(VueRouter);
-Vue.use(BootstrapVue);
+Vue.use('element-theme-chalk')
+Vue.use(Element, { size: 'small', zIndex: 3000 })
+Vue.use(VueRouter)
+library.add(faFacebookSquare, faInstagram, faEnvelope)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 //Set up Routes
 const routes = [
