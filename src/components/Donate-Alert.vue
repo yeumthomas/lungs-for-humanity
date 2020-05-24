95640d6bd7fd6461b42054627a41e325e83cdf7d
Thomas Yeum <thomasyeum@Thomas-MacBook-Pro.local>
2020-05-24 03:27:44 -0500

    Added Donate-Alert

---
+++ src/components/Donate-Alert.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="donate-alert">
+        <h3 class="donate-text">Click <a id="donate-button" href="/donate">here</a> to donate towards funding the ApolloBVM ventilator!</h3>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "Donate-Alert"
+    }
+</script>
+
+<style scoped>
+
+.donate-alert {
+    margin-top: 85px;
+    max-width: 650px;
+    background-color: rgb(243, 211, 239);
+    border-radius: 10px;
+    padding: 10px 14px 10px 14px;
+    max-width: 700px;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 10px;
+}
+
+#donate-button {
+    color: #e06666;
+}
+
+#donate-button:hover {
+    text-decoration: underline;
+}
+
+</style>
